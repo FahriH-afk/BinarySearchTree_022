423e646764b62831ece146227e03ac6e96ec6c6e
Fahri Hidayat <m.fahri.ft21@mail.umy.ac.id>
2022-12-26 17:17:04 +0700

    create classnode

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,9 +6,31 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
-    
-    internal class Program
+    //A Node Class Consi
+
+    class node
     {
+        public string info;
+        public node lchild;
+        public node rchild;
+
+        //Constructor for the node class
+
+        public node(string i, node l, node r)
+        {
+            info = i;
+            lchild = l;
+            rchild = r;
+        }
+    }
+
+    
+
+    class Program
+    {
+        
+
+
         static void Main(string[] args)
         {
         }
